478851cbabda08ba352caca6a0f0d582e4914a72
idleberg <git@idleberg.com>
2019-08-25 21:20:53 +0200

    change indentation

--- linter.py
+++ linter.py
@@ -45,17 +45,21 @@ class Makensis(Linter):
         cmd = ['makensis', '-V2']
 
         # Is strict mode?
-        if settings.get('strict') is True \
-        and '-WX' not in settings.get('args') \
-        and '/WX' not in settings.get('args'):
+        if (
+            settings.get('strict') is True
+            and '-WX' not in settings.get('args')
+            and '/WX' not in settings.get('args')
+        ):
             cmd.append('-WX')
 
         # Is PPO mode?
-        if settings.get('ppo') in [True, None] \
-        and '-PPO' not in settings.get('args') \
-        and '/PPO' not in settings.get('args') \
-        and '-SAFEPPO' not in settings.get('args') \
-        and '/SAFEPPO' not in settings.get('args'):
+        if (
+            settings.get('ppo') in [True, None] \
+            and '-PPO' not in settings.get('args') \
+            and '/PPO' not in settings.get('args') \
+            and '-SAFEPPO' not in settings.get('args') \
+            and '/SAFEPPO' not in settings.get('args')
+        ):
             cmd.append('-PPO')
             cmd.append('@')
         else:
